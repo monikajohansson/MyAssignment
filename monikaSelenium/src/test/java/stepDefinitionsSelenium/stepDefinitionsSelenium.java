c200789fa4cc79bfbe23589a45a756a8980c49ef
Monika Johansson <monikajohansson@live.se>
2021-03-18 16:05:24 +0100

    Starting project
    Created feature file
    
    Created TestRunner file
    
    Created empty StepDefinitions

---
+++ monikaSelenium/src/test/java/stepDefinitionsSelenium/stepDefinitionsSelenium.java
@@ -0,0 +1,39 @@
+package stepDefinitionsSelenium;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class stepDefinitionsSelenium {
+
+	@Given("I have logged in at Mailchimp")
+	public void i_have_logged_in_at_mailchimp() {
+
+	}
+
+	@Given("I have entered {string}")
+	public void i_have_entered(String email) {
+
+	}
+
+	@Given("I enter an username {string}")
+	public void i_enter_an_username(String userName) {
+
+	}
+
+	@Given("I enter a password {string}")
+	public void i_enter_a_password(String password) {
+
+	}
+
+	@When("I press signUp")
+	public void i_press_signUp() {
+
+	}
+
+	@Then("I verify the success in step")
+	public void i_verify_the_success_in_step() {
+
+	}
+
+}
---
+++ monikaSelenium/src/test/resources/features/createAccount.feature
@@ -0,0 +1,17 @@
+
+Feature: Create Account
+  I want to use this template to create accounts.
+
+  @tag2
+  Scenario Outline: Create an account successfully
+    Given I have logged in at Mailchimp
+    And I have entered <email>
+    And I enter an username <userName> 
+    And I enter a password <password>
+    When I press <signUp>
+    Then I verify the <status> in step
+
+    Examples: 
+      | email  | userName | password | signUp  | status  |
+      | "name"   | "userName" | "word"     | signUp | success |
+     
