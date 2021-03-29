c6e5cda664fae1c1b7f96a9161e6adeee16f98a9
Monika Johansson <monikajohansson@live.se>
2021-03-29 11:35:13 +0200

    Updated feature file added more methods to stepDefinitionsSelenium

--- monikaSelenium/src/test/java/stepDefinitionsSelenium/stepDefinitionsSelenium.java
+++ monikaSelenium/src/test/java/stepDefinitionsSelenium/stepDefinitionsSelenium.java
@@ -11,12 +11,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+import io.cucumber.core.cli.Main;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import net.bytebuddy.utility.RandomString;
 
 public class stepDefinitionsSelenium {
 	private WebDriver driver;
@@ -24,57 +23,57 @@ public class stepDefinitionsSelenium {
 	private String emailString;
 	private String password;
 
-	@Given("I have used {string} as browser")
-	public void browser(String browser) {
+	@Given("I am directed to MailChimp")
+	public void browser() {
 		DriveCreator creator = new DriveCreator();
-		driver = creator.createBrowser(browser);
+		driver = creator.createBrowser("chrome");
 
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	}
-
-	@And("I am directed to MailChimp")
-	public void mailChimp() {
 		driver.get("https://login.mailchimp.com/signup/");
 		driver.manage().window().maximize();
 	}
 
-	@And("I have entered {string}")
-	public void enterEmail(String email) {
-		sendEmail(By.id("email"), "keys");
+	@And("I have entered email")
+	public void enterEmail() {
+		sendEmail(By.id("email"));
 
 	}
 
-	@And("I enter an username {string}")
-	public void enterUserName(String userName) {
-		sendUserName(By.id("new_username"), "keys");
+	@And("I have entered NoEmail")
+	public void noEmail() {
+		
 	}
 
+	@When("I enter an userName")
+	public void enterUserName() {
+		sendUserName(By.id("new_username"));
+	}
+
+	@When("I enter an longUserName")
+	public void longUserName() {
+		sendLong(By.id("new_username"));
+	}
+	@When("I enter an usedUserName")
+	public void usedUserName() {
+		sendUsedUserName(By.id("new_username"));
+	}
 	@And("I enter a password {string}")
 	public void enterPassword(String password) {
-		sendPassword(By.id("new_password"), "keys");
+		sendPassword(By.id("new_password"));
 	}
 
-	
-
-	@When("I press signUp")
+	@And("I press signUp")
 	public void pressSignUp() {
 		click(By.id("create-account"));
 	}
 
-	
-
 	@Then("I verify the success in step")
 	public void verifySuccess() {
-     // Check your email
-		
+		// Check your email
+		// verifyEmail();
 		WebElement emailSent = driver.findElement(By.cssSelector("h1[class*=margin-bottom--lv3]"));
 		assertEquals("Check your email", emailSent.getText());
-		driver.quit();
-	}
-
-	@Given("I enter a long username {string}")
-	public void longUserName(String longUserName) {
-
+		// driver.quit();
 	}
 
 	private void click(By by) {
@@ -83,25 +82,43 @@ public class stepDefinitionsSelenium {
 
 	}
 
-	private void sendEmail(By by, String keys) {
+	private void sendEmail(By by) {
 		(new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(by));
 		rand = new Random();
-		int randomInt = rand.nextInt(10000);
+		int randomInt = rand.nextInt(1000000);
 		emailString = "username" + randomInt + "@yahoo.com";
 		driver.findElement(by).sendKeys(emailString);
 	}
 
-	private void sendUserName(By by, String keys) {
+	private void noEmail(By by, String noEmails) {
+		String hej = "";
+		driver.findElement(by).sendKeys(hej);
+	}
+
+	private void sendUserName(By by) {
 		(new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(by));
 
 		driver.findElement(by).sendKeys(emailString);
 	}
-	
-	private void sendPassword(By by, String keys) {
+
+	private void sendLong(By by) {
+		(new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(by));
+
+		driver.findElement(by).sendKeys(emailString
+				+ "1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111");
+	} // felmeddelande: Your username should either be an email address or consist of
+		// letters and numbers.
+
+	private void sendUsedUserName(By by) {
+		(new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(by));
+
+		driver.findElement(by).sendKeys("dragon");
+	} //felmeddelande:Another user with this username already exists. Maybe it's your evil twin. Spooky
+
+	private void sendPassword(By by) {
 		(new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(by));
 		password = "ThisIs@Password1";
 		driver.findElement(by).sendKeys(password);
 	}
 
-	
-	}
+}
--- monikaSelenium/src/test/resources/features/createAccount.feature
+++ monikaSelenium/src/test/resources/features/createAccount.feature
@@ -1,34 +1,23 @@
 
-Feature: Create Account
+Feature: Methods
   I want to use this template to create accounts.
 
   @tag2
   Scenario Outline: Create an account successfully
-  	Given I have used <browser> as browser
-    And I am directed to MailChimp
+
+		
+    Given I am directed to MailChimp 
     And I have entered <email>
-    And I enter an username <userName> 
+    When I enter an <userName>
     And I enter a password <password>
-    When I press <signUp>
+    And I press <signUp>
     Then I verify the <status> in step
 
     Examples: 
-      |browser  | email    | userName   | password   | signUp | status  |
-      |"chrome" |"name"    | "userName" | "word"     | signUp | success |
-      |"firefox"|"name"    | "userName" | "word"     | signUp | success |
-      |"edge"   |"name"    | "userName" | "word"     | signUp | success |
-
-  Scenario Outline: Create a username with more than 100 signs
-    Given I have used <browser> as browser
-    And I am directed to MailChimp
-    And I have entered <email>
-    And I enter a long username <userName> 
-    And I enter a password <password>
-    When I press <signUp>
-    Then I verify the <status> in step 
-    
-    Examples: 
-      |browser  | email    | userName   | password   | signUp | status  |
-      |"chrome" |"name"    | "userName" | "word"     | signUp | success |
-      |"firefox"|"name"    | "userName" | "word"     | signUp | success |
-      |"edge"   |"name"    | "userName" | "word"     | signUp | success |
+      | email     | userName       | password    | signUp | status  |
+      | NoEmail   | userName       | "word"      | signUp | success |
+      | email     | longUserName   | "word"      | signUp | success |
+      | email     | usedUserName   | "word"      | signUp | success |
+  		| email     | userName       | "word"      | signUp | success |
+      
+ 
