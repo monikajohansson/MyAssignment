00375a9ca702f0dcdcaf95080411760d37f64c2b
Monika Johansson <monikajohansson@live.se>
2021-03-31 17:33:39 +0200

    Changed creatAccount.feature
    Added steps to stepDefinitionsSelenium

--- monikaSelenium/src/test/java/stepDefinitionsSelenium/stepDefinitionsSelenium.java
+++ monikaSelenium/src/test/java/stepDefinitionsSelenium/stepDefinitionsSelenium.java
@@ -8,10 +8,10 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import io.cucumber.core.cli.Main;
+
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -22,6 +22,7 @@ public class stepDefinitionsSelenium {
 	private Random rand;
 	private String emailString;
 	private String password;
+	private String noEmailString;
 
 	@Given("I am directed to MailChimp")
 	public void browser() {
@@ -39,24 +40,32 @@ public class stepDefinitionsSelenium {
 
 	}
 
-	@And("I have entered NoEmail")
+	@And("I have entered noEmail")
 	public void noEmail() {
-		
+		sendNoEmail(By.id("email"));
 	}
 
-	@When("I enter an userName")
+	@When("I enter a userName")
 	public void enterUserName() {
 		sendUserName(By.id("new_username"));
 	}
 
-	@When("I enter an longUserName")
+	@When("I enter a longUserName")
 	public void longUserName() {
 		sendLong(By.id("new_username"));
 	}
-	@When("I enter an usedUserName")
+
+	@When("I enter a usedUserName")
 	public void usedUserName() {
 		sendUsedUserName(By.id("new_username"));
+
 	}
+
+	@When("I enter a newUserName")
+	public void noEmailUser() {
+		newUserName(By.id("new_username"));
+	}
+
 	@And("I enter a password {string}")
 	public void enterPassword(String password) {
 		sendPassword(By.id("new_password"));
@@ -67,13 +76,41 @@ public class stepDefinitionsSelenium {
 		click(By.id("create-account"));
 	}
 
+	@Then("I verify the failNoEmail in step")
+	public void verifyNoEmail() {
+
+		WebElement sentNoEmail = driver.findElement(By.cssSelector("span[class=invalid-error]"));
+		assertEquals("Please enter a value", sentNoEmail.getText());
+
+		driver.quit();
+	}
+
 	@Then("I verify the success in step")
 	public void verifySuccess() {
-		// Check your email
-		// verifyEmail();
+
 		WebElement emailSent = driver.findElement(By.cssSelector("h1[class*=margin-bottom--lv3]"));
 		assertEquals("Check your email", emailSent.getText());
-		// driver.quit();
+
+		driver.quit();
+	}
+
+	@Then("I verify the failLong in step")
+	public void verifyFailLong() {
+
+		WebElement sentLong = driver.findElement(By.cssSelector("span[class=invalid-error]"));
+		assertEquals("Enter a value less than 100 characters long", sentLong.getText());
+
+		driver.quit();
+	}
+
+	@Then("I verify the failUsed in step")
+	public void verifyFailUsed() {
+
+		WebElement sentUsed = driver.findElement(By.cssSelector("span[class=invalid-error]"));
+		assertEquals("Another user with this username already exists. Maybe it's your evil twin. Spooky.",
+				sentUsed.getText());
+
+		driver.quit();
 	}
 
 	private void click(By by) {
@@ -90,9 +127,10 @@ public class stepDefinitionsSelenium {
 		driver.findElement(by).sendKeys(emailString);
 	}
 
-	private void noEmail(By by, String noEmails) {
-		String hej = "";
-		driver.findElement(by).sendKeys(hej);
+	private void sendNoEmail(By by) {
+		(new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(by));
+		driver.findElement(by).sendKeys("");
+
 	}
 
 	private void sendUserName(By by) {
@@ -104,16 +142,24 @@ public class stepDefinitionsSelenium {
 	private void sendLong(By by) {
 		(new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(by));
 
-		driver.findElement(by).sendKeys(emailString
-				+ "1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111");
-	} // felmeddelande: Your username should either be an email address or consist of
-		// letters and numbers.
+		driver.findElement(by).sendKeys("11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111");
+
+	}
 
 	private void sendUsedUserName(By by) {
 		(new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(by));
 
 		driver.findElement(by).sendKeys("dragon");
-	} //felmeddelande:Another user with this username already exists. Maybe it's your evil twin. Spooky
+
+	}
+
+	private void newUserName(By by) {
+		(new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(by));
+		rand = new Random();
+		int randomInt = rand.nextInt(1000000);
+		noEmailString = "noemail" + randomInt + "@yahoo.com";
+		driver.findElement(by).sendKeys(noEmailString);
+	}
 
 	private void sendPassword(By by) {
 		(new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(by));
--- monikaSelenium/src/test/resources/features/createAccount.feature
+++ monikaSelenium/src/test/resources/features/createAccount.feature
@@ -3,21 +3,21 @@ Feature: Methods
   I want to use this template to create accounts.
 
   @tag2
-  Scenario Outline: Create an account successfully
+  Scenario Outline: Create an account
 
 		
     Given I am directed to MailChimp 
     And I have entered <email>
-    When I enter an <userName>
+    When I enter a <userName>
     And I enter a password <password>
     And I press <signUp>
     Then I verify the <status> in step
 
     Examples: 
       | email     | userName       | password    | signUp | status  |
-      | NoEmail   | userName       | "word"      | signUp | success |
-      | email     | longUserName   | "word"      | signUp | success |
-      | email     | usedUserName   | "word"      | signUp | success |
-  		| email     | userName       | "word"      | signUp | success |
+  		| noEmail   | newUserName       | "word"   | signUp | failNoEmail |
+      | email    | userName       | "word"       | signUp | success |
+      | email     | longUserName   | "word"      | signUp | failLong |
+      | email     | usedUserName   | "word"      | signUp | failUsed |
       
  
